0045552a62207656b6187d07102bb65fac8f1969
Whitney_vader <whitney@gmail.com>
2024-04-08 08:07:59 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.textContent = "YOUR-NAME is the champion";  
+newHeader.id = "victory";
+
+document.body.append(newHeader);
